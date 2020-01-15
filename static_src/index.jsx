a2f08eddc50131980ad4274525a8b47475294345
PinskyPhilipp <39645492+PinskyPhilipp@users.noreply.github.com>
2020-01-15 13:04:43 +0300

    Add files via upload

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Layout from './components/Layout';
+import { BrowserRouter } from 'react-router-dom';
+import Router from './components/Router'
+import { Provider } from 'react-redux';
+import initStore from './utils/store';
 
 ReactDOM.render(
-   <MuiThemeProvider>
-       <Layout />
-   </MuiThemeProvider>,
+    <Provider store={ initStore() }>
+        <BrowserRouter>
+            <MuiThemeProvider>
+                <Router />
+            </MuiThemeProvider>
+        </BrowserRouter>
+    </Provider>,
    document.getElementById('root'),
 );
